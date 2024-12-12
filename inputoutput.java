8c7a547fbb0872f44079a9a68dbadba476148d45
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-12-12 12:15:15 +0530

    Create inputoutput.java

---
+++ inputoutput.java
@@ -0,0 +1,31 @@
+// Sample Input
+
+// 42
+// 100
+// 125
+// Sample Output
+
+// 42
+// 100
+// 125
+
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       
+     
+     int a,b,c;
+     Scanner sc= new Scanner(System.in) ;
+    
+    a=sc.nextInt();
+    b=sc.nextInt();
+    c=sc.nextInt();
+    System.out.println(a+"\n"+b+"\n"+c);
+    
+     
+    }
+}
